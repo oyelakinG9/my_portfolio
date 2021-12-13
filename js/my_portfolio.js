8b25fff20345f7a1f2d3f09f72d931bb341b4a00
oyelakinG9 <oyelakinr37@gmail.com>
2021-12-13 22:56:53 +0100

    fix: linter fix

--- js/my_portfolio.js
+++ js/my_portfolio.js
@@ -1,7 +1,5 @@
 const navBar = document.querySelector('.navbar');
 
-const navBar = document.querySelector('.navbar');
-
 navBar.addEventListener('click', () => {
   navBar.classList.toggle('change');
 });
