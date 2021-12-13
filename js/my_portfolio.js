8c8994e31644bde666e540758bfa7c964e8c8a20
oyelakinG9 <oyelakinr37@gmail.com>
2021-12-13 22:34:06 +0100

    fix: linter fix

--- js/my_portfolio.js
+++ js/my_portfolio.js
@@ -1,8 +1,6 @@
-const menuIcon = document.querySelector(".hamburger-menu");
 
-const navBar = document.querySelector(".navbar");
+const navBar = document.querySelector('.navbar');
 
-const navList = document.querySelector('.nav-list');
 
 navBar.addEventListener('click', () => {
   navBar.classList.toggle('change');
